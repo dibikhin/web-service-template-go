0ebb5ca60b10286adeac613c8be89e7eae72b9c9
Roman Dibikhin <dibikhin@gmail.com>
2024-07-23 18:41:28 +0300

    simplified

--- internal/dummy/errors.go
+++ internal/dummy/errors.go
@@ -1,26 +1,15 @@
 package dummy
 
-import "errors"
-
-var (
-	ErrNotFound = errors.New("not found")
-)
-
 type NotFoundError struct {
-	Err     error
 	Message string
 }
 
 func NewNotFoundError(msg string) error {
 	return &NotFoundError{
-		Message: msg, Err: ErrNotFound,
+		Message: msg,
 	}
 }
 
 func (e *NotFoundError) Error() string {
 	return e.Message
-}
-
-func (e *NotFoundError) Is(target error) bool {
-	return errors.Is(e.Err, target)
 }
